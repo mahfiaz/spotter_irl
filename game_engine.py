1c2dc834b9db1f8c5e0db1975d10987a82be262a
Leevi Põldaru <leevi@nasys.no>
2017-04-22 00:26:09 +0300

    Static classes.

--- game_engine.py
+++ game_engine.py
@@ -22,57 +22,53 @@ cur = connection.cursor()
 
 class Round():
 
-    firstRun = True
+    def initOnce(cursor):
+        Round.cur = cursor
+        Round.createDataTable()
 
-    def __init__(self, cursor):
-        self.cur = cursor
-        if Round.firstRun:
-            self.createDataTable()
-            Round.firstRun = False
-
-    def createDataTable(self):
-        self.cur.execute("""CREATE TABLE round_data (
+    def createDataTable():
+        Round.cur.execute("""CREATE TABLE round_data (
             round_id serial PRIMARY KEY,
             round_name VARCHAR(30) NOT NULL,
             round_start TIMESTAMP,
             round_end TIMESTAMP)""")
 
-    def add(self, name, time_start, time_end):
-        self.cur.execute("""SELECT round_name
+    def add(name, time_start, time_end):
+        Round.cur.execute("""SELECT round_name
             FROM round_data 
             WHERE (round_start = %s OR (round_start < %s AND round_end > %s)) OR
             (round_end = %s AND (round_start < %s AND round_end > %s))""",
             (time_start, time_start, time_start, time_end, time_end, time_end))
-        if not self.cur.fetchall():
-            self.cur.execute("""INSERT INTO round_data (round_name, round_start, round_end)
+        if not Round.cur.fetchall():
+            Round.cur.execute("""INSERT INTO round_data (round_name, round_start, round_end)
                 VALUES (%s, %s, %s)""", (name, time_start, time_end))
             return True
         else:
             print("Error: New round has overlapping time. not added", name, time_start, time_end)
             return False
 
-    def getActive(self):
-        self.cur.execute("""SELECT round_id, round_name
+    def getActive():
+        Round.cur.execute("""SELECT round_id, round_name
             FROM round_data 
             WHERE (round_start <= NOW() AND round_end > NOW())""")
-        active = self.cur.fetchone()
+        active = Round.cur.fetchone()
         if active:
             id, name = active
             return id
         else:
             return False
 
-    def isActive(self):
-        if self.getActive():
+    def isActive():
+        if Round.getActive():
             return True
         else:
             return False
 
-    def print(self):
-        self.cur.execute("""SELECT round_id, round_name, round_start, round_end
+    def print():
+        Round.cur.execute("""SELECT round_id, round_name, round_start, round_end
             FROM round_data """)
-        rows = self.cur.fetchall()
-        active = self.getActive()
+        rows = Round.cur.fetchall()
+        active = Round.getActive()
         print("Rounds:")
         for row in rows:
             id, name, time1, time2 = row
@@ -81,7 +77,7 @@ class Round():
                 indicator = " * "
             print(indicator, id, name, time1, time2)
 
-    def addTestRounds(self):
+    def addTestRounds():
         time.strftime(dateformat)
         time1 = format(datetime.datetime.now() + datetime.timedelta(hours = -2), dateformat)
         time2 = format(datetime.datetime.now() + datetime.timedelta(hours = -1), dateformat)
@@ -91,26 +87,22 @@ class Round():
         dataDict = ({"name":"eelmine mäng", "time_start":time1, "time_end":time2},
                 {"name":"praegune mäng", "time_start":time2, "time_end":time3},
                 {"name":"järgmine mäng", "time_start":time3, "time_end":time4})
-        self.add("eelmine mäng", time1, time2)
-        self.add("praegune mäng", time2, time3)
-        self.add("järgmine mäng", time3, time4)
-        self.add("vale mäng", time2, time4)
+        Round.add("eelmine mäng", time1, time2)
+        Round.add("praegune mäng", time2, time3)
+        Round.add("järgmine mäng", time3, time4)
+        Round.add("vale mäng", time2, time4)
 
 
 
 
 class Player:
 
-    firstRun = True
+    def initOnce(cursor):
+        Player.cur = cursor
+        Player.createDataTable()
 
-    def __init__(self, cursor):
-        self.cur = cursor
-        if Player.firstRun:
-            self.createDataTable()
-            Player.firstRun = False
-
-    def createDataTable(self):
-        self.cur.execute("""CREATE TABLE player_data (
+    def createDataTable():
+        Player.cur.execute("""CREATE TABLE player_data (
             player_id serial PRIMARY KEY,
             player_name varchar(128) UNIQUE,
             player_mobile varchar(64) UNIQUE,
@@ -119,53 +111,49 @@ class Player:
             player_headshotcode_id int DEFAULT 0,
             player_created timestamp DEFAULT NOW() )""")
 
-    def add(self, name, mobile, email):
-        self.cur.execute("""SELECT (player_name, player_mobile, player_email)
+    def add(name, mobile, email):
+        Player.cur.execute("""SELECT (player_name, player_mobile, player_email)
             FROM player_data 
             WHERE player_name = %s OR player_mobile = %s OR player_email = %s""",
             (name, mobile, email))
-        rows = self.cur.fetchall()
+        rows = Player.cur.fetchall()
         if not rows:
-            self.cur.execute("""INSERT INTO player_data (player_name, player_mobile, player_email) VALUES (%s, %s, %s)""", (name, mobile, email))
+            Player.cur.execute("""INSERT INTO player_data (player_name, player_mobile, player_email) VALUES (%s, %s, %s)""", (name, mobile, email))
             print("Player added.", name, mobile, email)
             return True
         else:
             print("not entirely unique player. not added")
         return False
 
-    def print(self):
-        self.cur.execute("""SELECT * FROM player_data """)
-        rows = self.cur.fetchall()
+    def print():
+        Player.cur.execute("""SELECT * FROM player_data """)
+        rows = Player.cur.fetchall()
         print("Players:")
         for player in rows:
             print(" - ", player[1], player[2], player[3])
 
-    def addTestPlayers(self):
+    def addTestPlayers():
         dataDict = ({"name":"Ets2", "mobile":"550054", "email":"ets@gail.cm"},
                 {"name":"Vollts", "mobile":"5500547", "email":"Volts@gail.cm"},
                 {"name":"KalleLalle", "mobile":"581845", "email":"KAlli@gail.cm"})
         for each in dataDict:
-            self.add(each['name'], each['mobile'], each['email'])
+            Player.add(each['name'], each['mobile'], each['email'])
 
 class Team:
 
-    firstRun = True
+    def initOnce(cursor):
+        Team.cur = cursor
+        Team.createTeamTable()
+        Team.createTeamPlayersTable()
 
-    def __init__(self, cursor):
-        self.cur = cursor
-        if Team.firstRun:
-            self.createTeamTable()
-            self.createTeamPlayersTable()
-            Team.firstRun = False
-            
-    def createTeamTable(self):
-        self.cur.execute("""CREATE TABLE team (
+    def createTeamTable():
+        Team.cur.execute("""CREATE TABLE team (
             team_id serial PRIMARY KEY,
             team_name VARCHAR(30) NOT NULL,
             round_id int)""")
 
-    def createTeamPlayersTable(self):
-        self.cur.execute("""CREATE TABLE team_players (
+    def createTeamPlayersTable():
+        Team.cur.execute("""CREATE TABLE team_players (
             player_id int,
             team_id int,
             added timestamp)""")
@@ -174,54 +162,50 @@ class Team:
 
 class Code:
 
-    firstRun = True
+    def initOnce(cursor):
+        Code.cur = cursor
+        Code.createDataTable()
 
-    def __init__(self, cursor):
-        self.cur = cursor
-        if Code.firstRun:
-            self.createDataTable()
-            Code.firstRun = False
-
-    def createDataTable(self):
-        self.cur.execute("""CREATE TABLE shot_code (
+    def createDataTable():
+        Code.cur.execute("""CREATE TABLE shot_code (
             shot_id serial PRIMARY KEY,
             shot_value int unique,
             player_id int,
             added timestamp DEFAULT NOW())""")
 
-    def findShotCode(self, code):
-        self.cur.execute("""SELECT shot_id, player_id
+    def findShotCode(code):
+        Code.cur.execute("""SELECT shot_id, player_id
             FROM shot_code
             WHERE shot_value = %s""", [code])
-        found = self.cur.fetchone()
+        found = Code.cur.fetchone()
         if found:
             return found
         return False
 
-    def generateShotCode(self, playerId):
+    def generateShotCode(playerId):
         fail = True
         newCode = 0
         while fail:
             new_code = random.randint(1000,9999)
-            fail = self.findShotCode(newCode)
-        self.cur.execute("""INSERT INTO shot_code (shot_value, player_id)
+            fail = Code.findShotCode(newCode)
+        Code.cur.execute("""INSERT INTO shot_code (shot_value, player_id)
             VALUES (%s, %s)""", (newCode, playerId))
-        self.cur.execute("""SELECT shot_id
+        Code.cur.execute("""SELECT shot_id
             FROM shot_code 
             WHERE shot_value = %s""", [new_code])
         id = cur.fetchone()
-        self.cur.execute("""UPDATE player_data
+        Code.cur.execute("""UPDATE player_data
             SET player_shot_id = %s
             WHERE player_id = %s""", (id, playerId))
 
-    def findWhoWasShot(self, code):
-        found = self.findShotCode(code)
+    def findWhoWasShot(code):
+        found = Code.findShotCode(code)
         if found:
             shotId, playerId = found
-            self.cur.execute("""UPDATE shot_code
+            Code.cur.execute("""UPDATE shot_code
                 SET player_id = NULL
                 WHERE shot_id = %s""", shotId)
-            self.cur.execute("""UPDATE player_data
+            Code.cur.execute("""UPDATE player_data
                 SET player_shot_id = %s
                 WHERE player_id = %s""", (shotId, playerId))
             return playerId
@@ -240,17 +224,17 @@ def main():
 #    cursor = connection.cursor()
     cursor = cur
 
-    round = Round(cursor)
-    player = Player(cursor)
-    code = Code(cursor)
+    Round.initOnce(cursor)
+    Player.initOnce(cursor)
+    Code.initOnce(cursor)
 
-    round.addTestRounds()
-    player.addTestPlayers()
-    player.add("Vollts2", "3593", "ille2@gmail.ocom")
+    Round.addTestRounds()
+    Player.addTestPlayers()
+    Player.add("Vollts2", "3593", "ille2@gmail.ocom")
 
-    round.print()
-    print("round active", round.isActive())
-    player.print()
+    Round.print()
+    print("round active", Round.isActive())
+    Player.print()
 
 
 if __name__ == "__main__":
