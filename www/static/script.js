49092ad7a23b475ea3fb3c60b60acff7cce5f278
Rando Tomingas <randotm@users.noreply.github.com>
2017-05-27 12:56:09 +0300

    Events now showing correct time in base view aswell.

--- www/static/script.js
+++ www/static/script.js
@@ -44,7 +44,7 @@ function getAllEvents() {
                 for(i in events) {
                     event = events[i];
                     if (event["visible"] == "All") {
-                        divContents += "<p>"+event["time"];
+                        divContents += "<p>" + parseInt(event["time"].substring(0,2)) + 3 + event["time"].substring(2,5);
                         divContents += " <span style='color:#"+event["text1"]["color"]+";'>"+event["text1"]["text"]+"</span>";
                         divContents += " <span style='color:#"+event["text2"]["color"]+";'>"+event["text2"]["text"]+"</span>";
                         if (event["text3"]["text"] != null) {
